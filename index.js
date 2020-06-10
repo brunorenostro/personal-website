a52eb9748c8ccb265589d65e96d1c9ac58794005
btox <brunorenostro@gmail.com>
2020-06-10 14:57:43 -0400

    criação do meu site pessoal

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = 'Mayk';
+console.log(name);
+const aluno1=6;
+const aluno2=10;
+const aluno3=2;
+const notaAluno=8.8;
+const media=(aluno1+aluno2+aluno3+notaAluno)/4;
+if(media>=5){
+    console.log(`Parabéns vc foi aprovado com a nota ${media}`);
+}
+else console.log("Reprovado");
+console.log(typeof notaAluno);
